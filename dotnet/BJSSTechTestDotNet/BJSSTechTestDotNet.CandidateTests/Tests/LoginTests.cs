b4aecdab71d53d03bf61133115c6e28c176ed3a5
PhysicsJon <j.r.alsop.18@gmail.com>
2023-02-23 11:39:43 +0000

    Interview code

--- dotnet/BJSSTechTestDotNet/BJSSTechTestDotNet.CandidateTests/Tests/LoginTests.cs
+++ dotnet/BJSSTechTestDotNet/BJSSTechTestDotNet.CandidateTests/Tests/LoginTests.cs
@@ -1,4 +1,7 @@
-﻿using BJSSTechTestDotNet.CandidateTests.Utils;
+﻿using System.Linq;
+using BJSSTechTestDotNet.CandidateTests.Utils;
+using FluentAssertions;
+using FluentAssertions.Execution;
 using OpenQA.Selenium;
 using Xunit;
 
@@ -22,7 +25,70 @@ namespace BJSSTechTestDotNet.CandidateTests.Tests
 		public void LoginTest_ExampleTest()
 		{
 			// Provided here is an example of how the driver is set up and ready to use.
-			Driver.FindElement(By.Id("example_id"));
+			Driver.FindElement(By.Id("UserName")).SendKeys("candidate@bjss.com");
+			Driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("Test123");
+			Driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+			var textSuccessMessage = Driver.FindElement(By.XPath("//div[@class='alert alert-success']")).Text;
+			textSuccessMessage.Should().Be("Welcome to the confirmation screen");
+
+		}
+
+		// Invalid username
+			[Fact]
+			public void InvalidPassword()
+			{
+				Driver.FindElement(By.Id("UserName")).SendKeys("candidate@bjss.com");
+				Driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("Test12");
+				Driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+				Driver.FindElement(By.CssSelector("div.validation-summary-errors")).Displayed.Should().BeTrue();
+				Driver.FindElement(By.CssSelector("div.validation-summary-errors li")).Text.Should().Be("Invalid username/password");
+
+			}
+
+			[Fact]		// invalid password
+
+			public void InvalidUserNmae()
+			{
+				Driver.FindElement(By.Id("UserName")).SendKeys("Abc@bjss.com");
+				Driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("Test123");
+				Driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+				Driver.FindElement(By.CssSelector("div.validation-summary-errors")).Displayed.Should().BeTrue();
+				Driver.FindElement(By.CssSelector("div.validation-summary-errors li")).Text.Should().Be("Invalid username/password");
+
+			}
+		// empty username
+		[Fact]
+		public void EmptyUserName()
+		{
+				Driver.FindElement(By.Id("UserName")).SendKeys("");
+				Driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("Test123");
+				Driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+				Driver.FindElement(By.CssSelector("div.validation-summary-errors")).Displayed.Should().BeTrue();
+				Driver.FindElement(By.CssSelector("div.validation-summary-errors li")).Text.Should().Be("Email cannot be empty");
+				Driver.FindElement(By.CssSelector("span.field-validation-error")).Text.Should().Be("Email cannot be empty");			
+
+		}
+		// empty password
+		[Fact]
+		public void EmptyPassword()
+		{
+				Driver.FindElement(By.Id("UserName")).SendKeys("Abc@bjss.com");
+				Driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("");
+				Driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+				using(new AssertionScope()){
+					Driver.FindElement(By.CssSelector("div.validation-summary-errors")).Displayed.Should().BeTrue();
+					Driver.FindElement(By.CssSelector("div.validation-summary-errors li")).Text.Should().Be("Password cannot be empty");
+					Driver.FindElement(By.CssSelector("span.field-validation-error")).Text.Should().Be("Password cannot be empty");			
+				}
+		}
+
+		[Fact]
+		public void EmptyUserNamePassword()
+		{
+			Driver.FindElement(By.Id("UserName")).SendKeys("");
+				Driver.FindElement(By.XPath("//input[@type='password']")).SendKeys("");
+				Driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+				var errorSummaryErrors = Driver.FindElements(By.CssSelector("div.validation-summary-errors li")).Select(element => element.Text).ToList();
 		}
 	}
 }
